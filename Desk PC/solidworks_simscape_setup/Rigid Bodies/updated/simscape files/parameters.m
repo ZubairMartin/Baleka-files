dd06b7c5cd58d3e71c6c13db6fcd311017d866e5
Zubair Martin <74677130+ZubairMartin@users.noreply.github.com>
2022-09-18 15:06:46 +0200

    Delete Desk PC/solidworks_simscape_setup/Rigid Bodies/updated/simscape files directory

--- Desk PC/solidworks_simscape_setup/Rigid Bodies/updated/simscape files/parameters.m
+++
@@ -1,5 +0,0 @@
-%% Visual
-Boom.Base.color = [1.0 1.0 1.0 1.0]; % RGBA
-Boom.Center.color = [1.0 1.0 1.0 1.0]; % RGBA
-Boom.Pole.color = [1.0 1.0 1.0 1.0]; % RGBA
-Boom.End.color = [1.0 1.0 1.0 1.0]; % RGBA
